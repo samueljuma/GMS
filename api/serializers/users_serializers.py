53aa670cc63cb353b80b6418461046f418e4d2db
samueljuma <samueljuma1024@gmail.com>
2025-03-13 19:01:28 +0300

    fix typos

--- api/serializers/users_serializers.py
+++ api/serializers/users_serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from users.models import CustomUser
 
-class UserRegistrartionSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
--- api/views/users_views.py
+++ api/views/users_views.py
@@ -4,14 +4,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from api.serializers.users_serializers import UserRegistrartionSerializer
+from api.serializers.users_serializers import UserRegistrationSerializer
 
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        serializer = UserRegistrartionSerializer(data = request.data)
+        serializer = UserRegistrationSerializer(data = request.data)
         if serializer.is_valid():
             user = serializer.save()
             return Response(

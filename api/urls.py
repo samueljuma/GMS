1f902f116372377f99a2fc7229d839f8d02b6fa2
samueljuma <samueljuma1024@gmail.com>
2025-03-14 23:16:17 +0300

    implement logic to pass refresh token in header for refresh endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from api.views.users_views import RegisterView, LoginView
+from api.views.users_views import RegisterView, LoginView, CustomTokenRefreshView
 from rest_framework_simplejwt.views import TokenRefreshView
+
 
 
 urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/login/", LoginView.as_view(), name="login"),
-    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),  # Refresh Token
+    path("auth/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),  # Refresh Token
 ]
--- api/views/users_views.py
+++ api/views/users_views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from api.serializers.users_serializers import UserRegistrationSerializer, LoginSerializer
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 
 
 class RegisterView(APIView):
@@ -28,6 +30,21 @@ class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)  # Raises exception if invalid
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.headers.get("Refresh-Token")  # Extract from headers
+
+        if not refresh_token:
+            return Response(
+                {"error": "Refresh token missing"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = TokenRefreshSerializer(data={"refresh": refresh_token})
+        serializer.is_valid(raise_exception=True)
+
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 
